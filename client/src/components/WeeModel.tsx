bbca8df784bb2e0ccc1ad210584917200fdf7039
aatoaa <artolle@pm.me>
2023-07-29 10:27:42 +0200

    refactor: model loading

--- client/src/components/WeeModel.tsx
+++ client/src/components/WeeModel.tsx
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
-import { GLTFResult, scale } from './WeeTypes';
+import { scale } from './WeeTypes';
 
 export interface IWeeModel {
   glb: string;
@@ -12,24 +12,29 @@ export interface IWeeModel {
 function WeeModel({ glb, scale }: IWeeModel) {
   const meshRef = useRef<THREE.Mesh>(null!);
   useFrame((state, delta) => (meshRef.current.rotation.x += delta / 3));
-  const gltf = useGLTF(glb) as GLTFResult;
-  const model = setupModel(gltf);
+  const { scene } = useGLTF(glb);
+  const model = setupModel(scene);
   const geometry = model.geometry;
 
   const modelScale = scale.$numberDecimal ?? 1;
 
   return (
     geometry && (
-      <mesh geometry={geometry} ref={meshRef} scale={modelScale}>
+      <mesh
+        geometry={geometry}
+        ref={meshRef}
+        scale={modelScale}
+        position={[0, 0, 0]}
+      >
         <meshStandardMaterial color={'rgb(40,40,40)'} />
       </mesh>
     )
   );
 }
 
-function setupModel(data: GLTFResult) {
+function setupModel(data: THREE.Group) {
   let model = null;
-  const child = data.scene.children[0];
+  const child = data.children[0];
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   model = child.isMesh && child;
